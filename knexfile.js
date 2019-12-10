d60169f7f2fc504fbca40a1c9fc82eb9242099c6
Zac Higgins <zachiggins@Higgins-iMac.local>
2019-12-10 14:30:16 -0700

    updated dependencies. Created knex file and migrations file for cars table

---
+++ knexfile.js
@@ -0,0 +1,13 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {
+      filename: './dev.sqlite3'
+    }
+  }
+
+};
