ca7ca1507f4c944b0290f27fd2bf3b1edd4c18b6
Zac Higgins <zachiggins@Higgins-iMac.local>
2019-12-10 15:29:54 -0700

    added proper file paths to knex file. rebuilt migrations for cars table. Created seed data

--- knexfile.js
+++ knexfile.js
@@ -6,7 +6,13 @@ module.exports = {
     client: 'sqlite3',
     useNullAsDefault: true,
     connection: {
-      filename: './dev.sqlite3'
+      filename: "./data/cars.db3" // where is the database file?
+    },
+    migrations: {
+      directory: "./data/migrations" // will be created automatically
+    },
+    seeds: {
+      directory: "./data/seeds" // also created automatically
     }
   }
 
